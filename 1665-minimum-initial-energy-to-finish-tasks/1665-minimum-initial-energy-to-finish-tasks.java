2b19f0cdd13b8b4708bf3db4db155bc2ba8a21d9
CaptainHarsh1 <139635891+CaptainHarsh1@users.noreply.github.com>
2026-05-12 20:20:02 +0530

    Time: 22 ms (100%), Space: 114.2 MB (14.42%) - LeetHub

---
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, (a, b) -> a[1] - a[0] - (b[1] - b[0]));
+        int ans = 0;
+        for (int[] task : tasks) {
+            ans = Math.max(ans + task[0], task[1]);
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
