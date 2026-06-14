cee75fd6f9fa7595ee9f3a334ba2b31d0a456c1e
CaptainHarsh1 <139635891+CaptainHarsh1@users.noreply.github.com>
2026-06-14 22:16:08 +0530

    Time: 20 ms (7.28%), Space: 107.4 MB (23.18%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        Stack<Integer> half = new Stack<>();
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            half.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        int res = 0;
+        while (slow != null) {
+            res = Math.max(res, half.pop() + slow.val);
+            slow = slow.next;
+        }
+
+        return res;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
